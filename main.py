973513d53663ff27281541328a2713079243808c
Evie Durdin <eviedurdin@Evies-Laptop.local>
2025-04-28 20:47:50 +0930

    create methods for habit class

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
+from datetime import datetime
+
+
 class Habit():
-    def __init__(self, name, description, streak, dates_completed):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.streak = streak
-        self.dates_completed = dates_completed
+        self.streak = 0
+        self.dates_completed = []
 
-    def mark_complete(date):
+    def mark_complete(self, date):
+        if date not in self.dates_completed:
+            self.dates_completed.append(date)
+            print(f"{self.name} completed on: {date}")
+            streak += 1
+        else:
+            print("You've already completed this habit today!")
+
+    def reset_streak(self):
+        ex_streak = self.streak
+        self.streak = 0
+        print(
+            f"Your streak for '{self.name}' has been reset from {ex_streak} to 0")
+
+    def get_streak(self):
+        print(f"Your current streak for '{self.name}' is: {self.streak}")
+
+    def get_completion_rate(self):
         pass
 
-    def reset_streak():
-        pass
 
-    def get_streak():
-        pass
-
-    def get_completion_rate():
-        pass
+habit1 = Habit('Run', 'Go for a run')
+habit1.mark_complete(datetime.now().date())
 
 
 class User():
