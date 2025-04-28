7f45cbfbaf7df3a28b4179064314d9264518e23a
Evie Durdin <eviedurdin@Evies-Laptop.local>
2025-04-28 20:14:46 +0930

    create classes

---
+++ main.py
@@ -0,0 +1,59 @@
+class Habit():
+    def __init__(self, name, description, streak, dates_completed):
+        self.name = name
+        self.description = description
+        self.streak = streak
+        self.dates_completed = dates_completed
+
+    def mark_complete(date):
+        pass
+
+    def reset_streak():
+        pass
+
+    def get_streak():
+        pass
+
+    def get_completion_rate():
+        pass
+
+
+class User():
+    def __init__(self, username):
+        self.username = username
+        self.habits = []
+
+    def add_habit():
+        pass
+
+    def remove_habit():
+        pass
+
+    def get_habit(name):
+        pass
+
+    def view_summary():
+        pass
+
+
+class HabitTrackerApp():
+    def __init__(self, users):
+        self.users = users
+
+    def load_data():
+        pass
+
+    def save_data():
+        pass
+
+    def run():
+        pass
+
+    def login():
+        pass
+
+    def register():
+        pass
+
+    def display_menu():
+        pass
